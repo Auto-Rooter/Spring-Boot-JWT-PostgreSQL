434d483368d2f5acdcd73b78395a6c50e77665e6
Autorooter <hadi.assalem@gmail.com>
2020-07-19 22:09:14 +0200

    project created , database schema

---
+++ src/test/java/com/autorooter/apiTracker/ApiTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autorooter.apiTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
